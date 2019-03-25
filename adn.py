32d5e48db73cfa1268bb2df21cf40a5059176fb3
Johan Andres Cortes Pita <jcortes71058@universidadean.edu.co>
2019-03-24 21:45:34 -0500

    Se asignan las funciones para cada miembro del grupo

--- adn.py
+++ adn.py
@@ -1,50 +1,99 @@
+"""
+Desarrollo: Edward
+Pruebas: Johan
+Documentación: David
+"""
 def obtener_complemento(base):
     # retorna caracter
     pass
 
-
+"""
+Desarrollo: Edward
+Pruebas: Johan
+Documentación: David
+"""
 def generar_cadena_complementaria(adn):
     pass
 
-
+"""
+Desarrollo: Edward
+Pruebas: Johan
+Documentación: David
+"""
 def calcular_correspondencia(adn1, adn2):
     # retorna num
     pass
 
-
+"""
+Desarrollo: Edward
+Pruebas: Johan
+Documentación: David
+"""
 def corresponden(adn1, adn2):
     # retorna Bool
     pass
 
-
+"""
+Desarrollo: David
+Pruebas: Edward
+Documentación: Johan
+"""
 def es_cadena_valida(adn):
     pass
 
-
+"""
+Desarrollo: David
+Pruebas: Edward
+Documentación: Johan
+"""
 def es_base(caracter):
     pass
 
-
+"""
+Desarrollo: David
+Pruebas: Edward
+Documentación: Johan
+"""
 def es_subcadena(adn1, adn2):
     pass
 
-
+"""
+Desarrollo: David
+Pruebas: Edward
+Documentación: Johan
+"""
 def reparar_dano(adn, base):
     pass
 
-
+"""
+Desarrollo: Johan
+Pruebas: David
+Documentación: Edward
+"""
 def obtener_secciones(adn, n):
     pass
 
-
+"""
+Desarrollo: Johan
+Pruebas: David
+Documentación: Edward
+"""
 def obtener_complementos(lista_adn):
     pass
 
-
+"""
+Desarrollo: Johan
+Pruebas: David
+Documentación: Edward
+"""
 def unir_cadena(lista_adn):
     pass
 
-
+"""
+Desarrollo: Johan
+Pruebas: David
+Documentación: Edward
+"""
 def complementar_cadenas(lista_adn):
     pass
 
