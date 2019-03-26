18df48bfde816094af111c046fad2d242d98004c
David <wvillar85769@universidadean.edu.co>
2019-03-25 23:08:49 -0500

    Añado documentación de la parte 1, 2 , 3 y 4.

--- adn.py
+++ adn.py
@@ -1,18 +1,48 @@
 def obtener_complemento(base):
+    """
+    (str) -> str
+    'Recibe una letra y retorna su complemento'
+
+    :param base: Un valor de una cadena de ADN
+    :return: El complemento de la candena del ADN
+    """
     # retorna caracter
     pass
 
 
 def generar_cadena_complementaria(adn):
+    """
+    (str) ->  boolean
+    'Recibe una cadena de ADN y se retorna el complento de dicha cadena'
+
+    :param adn: Se recibe una cadena
+    :return: El complemento de dicha cadena
+    """
     pass
 
 
 def calcular_correspondencia(adn1, adn2):
+    """
+    (string, string)->number
+
+    Funcion que calcula el porcentaje de correspondencia de una cadena y otra
+    :param adn1: La primera cadena que voy a comparar
+    :param adn2 La segunda cadena que voy a comprar
+    :return: numbre: el porcentaje de acertividad o un mensaje
+    """
     # retorna num
     pass
 
 
 def corresponden(adn1, adn2):
+    """
+    str -> boolean
+
+    'recibe dos bases y verifica si la una es el complemento de la otra'
+    :param adn1: Se introduce primer parametro
+    :param adn2: Se introduce segundo parametro
+    :return: Se retorna si son complementos o no
+    """
     # retorna Bool
     pass
 
