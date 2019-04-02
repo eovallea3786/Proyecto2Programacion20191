744d5e242ff2702b5515831f83c11e8226223a50
Johan Andres Cortes Pita <42195246+JohanCortesPita@users.noreply.github.com>
2019-04-02 17:43:36 -0500

    Se agrega el desarrollo de las funciones9,10,11,12

--- adn.py
+++ adn.py
@@ -13,7 +13,7 @@ def obtener_complemento(base):
         return 'G'
     # de los contrario retorna que la letra no es una base
     else:
-        raise ValueError(base + " No es una base")
+        raise ValueError(base + " no es una base")
 
 def generar_cadena_complementaria(adn):
     # Obteniendo el complento de una cadena de ADN
@@ -27,11 +27,30 @@ def generar_cadena_complementaria(adn):
 
 def calcular_correspondencia(adn1, adn2):
     # Verificando si la base dos es el complemento de la base uno.
-    return adn1 == obtener_complemento(adn2)
+    # Declaramos desde un principio el valor porcentaje en 0
+    porcentaje = 0;
+    # Declaramos declaramos una posicion para poder acceder a la cadena dos en la posicion en la que este cadena_1
+    posicion = 0;
+
+    # validamos que las cadenas tengan la misma longitud
+    if (len(adn1) == len(adn2)):
+        # recorremos la cadena uno
+        for i in adn1:
+            # validamos de que posicion de cadena uno en cadena dos sea base
+            if (corresponden(i, adn2[posicion])):
+                # si es base, suma una unidad al porcentaje
+                porcentaje += 1
+            # a lo que termine la validacion, pasa a la siguiente posicion
+            posicion += 1
+        # retornamos el porcentaje
+        return porcentaje * 100 / len(adn1)
+    # si las cadenas no tienen la misma longitud, retorna um mensaje informando que no tienen la misma longitud
+    return 'Las cadenas no tienen la misma longitud'
 
 def corresponden(adn1, adn2):
     # retorna Bool
-    pass
+    # Verificando si la base dos es el complemento de la base uno.
+    return adn1 == obtener_complemento(adn2)
 
 
 def es_cadena_valida(adn):
@@ -54,40 +73,66 @@ def es_base(caracter):
 
 def es_subcadena(adn1, adn2):
 
-    if not es_cadena_valida(adn2):
-        raise ValueError('NO NUMEROS')
-    if not es_cadena_valida(adn1):
-        raise ValueError('NO NUMEROS')
     if adn2 in adn1:
         return True
-    elif adn2 not in adn1:
+    else:
         return False
 
 
 def reparar_dano(adn, base):
 
-    if int == type(complementaria):
-        raise TypeError('LOS NUMEROS NO SON VALIDOS')
-    if not es_cadena_valida(complementaria):
-        raise TypeError(complementaria + ' NO ES UNA CADENA VALIDA')
-    if corresponden(adn, complementaria):
-        return "LAS CADENAS CONCUERDAN"
-    elif not corresponden(adn, complementaria):
-        return generar_cadena_complementaria(adn)
+    if not es_base(complementaria):
+        Exception("La base ingresada para reparar dano no es una base")
+    
+    reparado = ""
+    for caracter in adn:
+        if es_base(caracter):
+            reparado = reparado + caracter
+        else:
+            reparado = reparado + caracter
+    return reparado
 
 
 def obtener_secciones(adn, n):
-    pass
+    if not es_cadena_valida(adn):
+        raise TypeError('La cadena no es valida')
+
+    suma_secciones = len(adn) // n
+    division_seccion = []
+
+    for secciones in range(n):
+        resultado = ""
+        cantidad = suma_secciones
+        if secciones == n - 1 and len(adn) % n != 0:
+            cantidad = suma_secciones + len(adn) % n
+        for caracter in range(cantidad):
+            base_cadena = secciones * suma_secciones + caracter
+            resultado = resultado + adn[base_cadena]
+        division_seccion.append(resultado)
+
+    return division_seccion
 
 
 def obtener_complementos(lista_adn):
-    pass
+    complemento = []
+    for cadena in lista_adn:
+        complemento.append(generar_cadena_complementaria(cadena))
+    return complemento
 
 
 def unir_cadena(lista_adn):
-    pass
+    cadena = ""
+    for cadenas in lista_adn:
+        for caracter in cadenas:
+            cadena = cadena + caracter
+    return cadena
 
 
 def complementar_cadenas(lista_adn):
-    pass
+    lista_complemento = ''
+
+    for complemento in lista_adn:
+        lista_complemento += generar_cadena_complementaria(complemento)
+    return lista_complemento
+
 
