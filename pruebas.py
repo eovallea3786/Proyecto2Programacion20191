8b50ae742b6c2116b5a9b30852635a3f63a2457d
Johan <jcortes71058@universidadean.edu.co>
2019-04-02 18:36:40 -0500

    Se agregan Pruebas Unitarias

--- pruebas.py
+++ pruebas.py
@@ -9,4 +9,12 @@ class pruebas(unittest.TestCase):
         self.assertEqual(f.obtener_complemento('T'), 'A')
         self.assertRaises(ValueError, f.obtener_complemento, 'Z')
     def test_generar_cadena_complementaria(self):
-        self.ass
+        self.assertEqual(f.generar_cadena_complementaria('ATGC'), 'TACG')
+        self.assertEqual(f.generar_cadena_complementaria('GATC'), 'CTAG')
+        self.assertEqual(f.generar_cadena_complementaria('CA'), 'GT')
+    def test_calcular_correspondencia(self):
+        self.assertEqual(f.calcular_correspondencia('ATATTACGGC', 'TATAATGCCG'), 100.0)
+        self.assertEqual(f.calcular_correspondencia('ATATATCGGC', 'TATAATGCCG'),  80.0)
+        self.assertEqual(f.calcular_correspondencia('ATATATCGGC', 'CGATTTACGA'), 20.0)
+        self.assertEqual(f.calcular_correspondencia('TTGGAACC', 'ACTA'), 'Las cadenas no tienen la misma longitud')
+
