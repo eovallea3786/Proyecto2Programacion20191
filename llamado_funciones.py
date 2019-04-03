09d021fc6c357ebccfe46ddfa0c221458b3849f9
Johan <jcortes71058@universidadean.edu.co>
2019-04-02 19:44:39 -0500

    Se adiciona el bono del proyecto

--- llamado_funciones.py
+++ llamado_funciones.py
@@ -1,13 +1,13 @@
-from Proyecto2 import adn as funciones
+import adn as funciones
 
 print("Esto Te permitira saber el porcentaje de correspondencia entre dos cadenas de ADN")
 cadena_texto = input("Por favor agregue dos cadenas de ADN con la misma longitud y separadas por ',' (No escriba las comillas, no seamos tontos): \n")
 
-archivo = open('archivo.txt','w')
+archivo = open('archivo.txt', 'w')
 archivo.write(cadena_texto+"\n")
 archivo.close()
-adn = open('archivo.txt','r')
+adn = open('archivo.txt', 'r')
 texto = adn.readlines()
 for ln in texto:
     cadenas = ln.strip().split(',')
-print(funciones.calcu(cadenas[0],cadenas[1]))
+print(funciones.calcular_correspondencia(cadenas[0], cadenas[1]))
