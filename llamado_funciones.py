ce82c388dcce41df785d29414102999efd87bf0f
Johan <jcortes71058@universidadean.edu.co>
2019-04-02 19:56:32 -0500

    Se adiciona el bono corregido del proyecto

--- llamado_funciones.py
+++ llamado_funciones.py
@@ -1,7 +1,7 @@
 import adn as funciones
 
-print("Esto Te permitira saber el porcentaje de correspondencia entre dos cadenas de ADN")
-cadena_texto = input("Por favor agregue dos cadenas de ADN con la misma longitud y separadas por ',' (No escriba las comillas, no seamos tontos): \n")
+print("Esto Te permitira saber el saber el complemento de una cadena de ADN")
+cadena_texto = input("Por favor agregue una cadena de ADN con la misma longitud y separadas por ',' (No escriba las comillas): \n")
 
 archivo = open('archivo.txt', 'w')
 archivo.write(cadena_texto+"\n")
@@ -10,4 +10,5 @@ adn = open('archivo.txt', 'r')
 texto = adn.readlines()
 for ln in texto:
     cadenas = ln.strip().split(',')
-print(funciones.calcular_correspondencia(cadenas[0], cadenas[1]))
+print(funciones.es_cadena_valida(cadena_texto))
+print(funciones.obtener_complementos(cadena_texto))
