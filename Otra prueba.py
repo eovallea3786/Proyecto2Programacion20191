8a51323c84f2ab9e125aef4a7d75b2adceabad5d
Johan <jcortes71058@universidadean.edu.co>
2019-04-04 19:48:40 -0500

    Se corrigen los dos bonos

--- Otra prueba.py
+++ Otra prueba.py
@@ -3,13 +3,13 @@ import adn as funciones
 print("Esto Te permitira saber el saber el complemento de una cadena de ADN")
 cadena_texto = input("Por favor agregue una cadena de ADN con la misma longitud y separadas por ',' (No escriba las comillas): \n")
 
-archivo = open('archivo.txt', 'w')
+archivo = open('archivo2.txt', 'w')
 archivo.write(cadena_texto+"\n")
 archivo.close()
-adn = open('archivo.txt', 'r')
+adn = open('archivo2.txt', 'r')
 texto = adn.readlines()
-for ln in texto:
-    cadenas = ln.strip().split(',')
-    print(funciones.calcular_correspondencia(cadenas[0], [1]))
 
-
+for i in adn:
+    cadenas = i.strip().split(',')
+print(funciones.es_cadena_valida(cadena_texto))
+print(funciones.obtener_complementos(cadena_texto))
